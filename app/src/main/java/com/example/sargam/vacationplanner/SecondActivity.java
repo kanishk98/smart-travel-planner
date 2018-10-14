c31354225a403d70b9f3243ecb341edbaf843e30
kanishk98 <kanishk.kakar@gmail.com>
2018-10-15 01:25:35 +0530

    Changed selectionArgs to include all calendars

--- app/src/main/java/com/example/sargam/vacationplanner/SecondActivity.java
+++ app/src/main/java/com/example/sargam/vacationplanner/SecondActivity.java
@@ -44,8 +44,8 @@ public class SecondActivity extends AppCompatActivity {
     private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
     private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
     private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
-    private static String calendarEmail;
-    private static String accountType;
+    private static String calendarEmail = "akakkar656@gmail.com";
+    private static String accountType = "com.google.calendar";
 
     @Override
     protected void onStart() {
@@ -77,7 +77,7 @@ public class SecondActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+        cur = cr.query(uri, EVENT_PROJECTION, CalendarContract.Calendars.VISIBLE + " = 1", null, CalendarContract.Calendars._ID + " ASC");
         // Use the cursor to step through the returned records
         while (cur.moveToNext()) {
             Log.d(TAG, "inside while loop");
