ca256a2e2cf88ad0aa5b80051e2c1a6e3e34a0e6
kanishk98 <kanishk.kakar@gmail.com>
2018-11-22 09:52:53 +0530

    Fix response issues

--- server/index.js
+++ server/index.js
@@ -7,20 +7,22 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.get('/predict', (req, res) => {
+app.get('/predict', async(req, res) => {
 	const quarter = req.query.quarter;
 	const age = req.query.age;
 	const gender = req.query.gender;
 	const duration = req.query.duration;
 	const budget = req.query.budget;
-	fetch('http://localhost:5000/predict?quarter=' + quarter + '&age=' + age + '&gender=' + gender + '&duration=' + duration + '&budget=' + budget)
-	.then(res => {
-		console.log(res);
+	const data = await fetch('http://localhost:5000/predict?quarter=' + quarter + '&age=' + age + '&gender=' + gender + '&duration=' + duration + '&budget=' + budget)
+	/*.then(suc => {
+		console.log(suc);
+		res.send(suc.json());
 	})
 	.catch(err => {
-		console.log(err);
-	})
+		res.send(err);
+	})*/
+	res.send(await data.json());
 });
-app.listen(3000, () => {
+app.listen(5001, () => {
 	console.log('Server online');
 })
