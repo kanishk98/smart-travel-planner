929a1a262d3dd7317c6ab13a3ca507ef06475131
kanishk98 <kanishk.kakar@gmail.com>
2018-11-22 09:26:13 +0530

    Create remote server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.get('/predict', (req, res) => {
+	const quarter = req.query.quarter;
+	const age = req.query.age;
+	const gender = req.query.gender;
+	const duration = req.query.duration;
+	const budget = req.query.budget;
+	fetch('http://localhost:5000/predict?quarter' + quarter + '&age=' + age + '&gender=' + gender + '&duration' + duration + '&budget' + budget)
+	.then(res => {
+		console.log(res);
+	})
+	.catch(err => {
+		console.log(err);
+	})
+});
+app.listen(3000, () => {
+	console.log('Server online');
+})
