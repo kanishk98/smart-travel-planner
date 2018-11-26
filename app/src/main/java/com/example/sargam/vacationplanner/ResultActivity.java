ef20bccebc7f75cd25b9cdc99e501c164acfad89
kanishk98 <kanishk.kakar@gmail.com>
2018-11-26 08:35:09 +0530

    Demo ready

--- app/src/main/java/com/example/sargam/vacationplanner/ResultActivity.java
+++ app/src/main/java/com/example/sargam/vacationplanner/ResultActivity.java
@@ -1,14 +1,24 @@
 package com.example.sargam.vacationplanner;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.StringTokenizer;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -21,6 +31,40 @@ public class ResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_result);
         resultA = getIntent().getStringArrayExtra("params");
         listview=findViewById(R.id.lvMain);
+        class GetPreds extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                String predictionsRead = null;
+                String url = "http://18.188.218.137/predict?quarter=1&age=20&gender=male&duration=40&budget="+resultA[2];
+                try {
+                    URLConnection connection = new URL(url).openConnection();
+                    InputStream response = connection.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(response));
+                    predictionsRead = br.readLine();
+                    Log.d("predictions", predictionsRead);
+                    predictionsRead = predictionsRead.substring(1, predictionsRead.length() - 1);
+                    StringTokenizer st = new StringTokenizer(predictionsRead, ",");
+                    int i = 0;
+                    while(st.hasMoreTokens()) {
+                        resultA[i] = st.nextToken();
+                        Log.d("predictions", resultA[i]);
+                        i = i + 1;
+                    }
+                    Log.d("ResultActivity", predictionsRead);
+                } catch (IOException ioException) {
+                    Log.d("RA", ioException.getMessage());
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                setupList();
+            }
+        }
+        new GetPreds().execute();
         setupList();
     }
 
